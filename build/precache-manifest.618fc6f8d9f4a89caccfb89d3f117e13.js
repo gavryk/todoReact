814289647ccd5a31b610baa1d54345667909f9ca
Oleg <gvozdyk23@gmail.com>
2019-05-29 23:12:40 +0300

    build

---
+++ build/precache-manifest.618fc6f8d9f4a89caccfb89d3f117e13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed5dce8531b64112cd31274d7c7610d",
+    "url": "/todoReact/index.html"
+  },
+  {
+    "revision": "e8bf5a9b3f650326dd7e",
+    "url": "/todoReact/static/css/main.362a4ff9.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/todoReact/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "e8bf5a9b3f650326dd7e",
+    "url": "/todoReact/static/js/main.8ab81462.chunk.js"
+  },
+  {
+    "revision": "9cc3f70042568672762f",
+    "url": "/todoReact/static/js/runtime~main.bf1ecc24.js"
+  }
+]);
